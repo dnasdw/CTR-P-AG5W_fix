3357076ed74e6b490154ef068ecb5c99d542b570
Sun Daowen <sundaowen@gmail.com>
2019-03-28 17:21:39 +0800

    update CTR-P-AG5W

--- CTR-P-AG5W/3-1_import_zh-Hant.bat
+++ CTR-P-AG5W/3-1_import_zh-Hant.bat
@@ -2,8 +2,8 @@ PUSHD "%~dp0"
 RD /S /Q build
 MD build\cxi0\exefs
 XCOPY cci\cxi0\exefs build\cxi0\exefs /S /Y
-MD build\cxi0\romfs
-XCOPY cci\cxi0\romfs build\cxi0\romfs /S /Y
+REM MD build\cxi0\romfs
+REM XCOPY cci\cxi0\romfs build\cxi0\romfs /S /Y
 tools\3dstool --lock -vf build\cxi0\exefs\code.bin --region TWN --language TW || PAUSE
 REM FOR /R build\cxi0\exefs\banner %%I IN (*.bcmdl) DO (tools\txobtool -ivfd "%%~I" "%%~dpnI" || PAUSE)
 FOR /L %%I IN (1,1,16) DO (COPY build\cxi0\exefs\banner\banner16.bcmdl build\cxi0\exefs\banner\banner%%I.bcmdl)
--- CTR-P-AG5W/4-1_create_zh-Hant.bat
+++ CTR-P-AG5W/4-1_create_zh-Hant.bat
@@ -1,15 +1,17 @@
 PUSHD "%~dp0"
 tools\3dstool -cvtf banner build\cxi0\exefs\banner.bnr --banner-dir build\cxi0\exefs\banner
 tools\3dstool -cvtfz exefs build\cxi0\exefs.bin --header build\cxi0\exefs\exefsheader.bin --exefs-dir build\cxi0\exefs
-tools\3dstool -cvtf romfs build\cxi0\romfs.bin --romfs-dir build\cxi0\romfs --romfs cci\cxi0\romfs.bin
-MD build\cfa1
-tools\3dstool -cvtf romfs build\cfa1\romfs.bin --romfs-dir cci\cfa1\romfs --romfs cci\cfa1\romfs.bin
-MD build\cfa7
-tools\3dstool -cvtf romfs build\cfa7\romfs.bin --romfs-dir cci\cfa7\romfs --romfs cci\cfa7\romfs.bin
-tools\3dstool -cvtf cxi build\0.cxi --header cci\cxi0\ncchheader.bin --exh cci\cxi0\exh.bin --logo cci\cxi0\logo.darc.lz --plain cci\cxi0\plain.bin --exefs build\cxi0\exefs.bin --romfs build\cxi0\romfs.bin
-tools\3dstool -cvtf cfa build\1.cfa --header cci\cfa1\ncchheader.bin --romfs build\cfa1\romfs.bin
-tools\3dstool -cvtf cfa build\7.cfa --header cci\cfa7\ncchheader.bin --romfs build\cfa7\romfs.bin
-tools\3dstool -cvt017f cci build\0.cxi build\1.cfa build\7.cfa "Paper Mario - Sticker Star (TWN) (zh-Hant).3ds" --header cci\ncsdheader.bin
+REM tools\3dstool -cvtf romfs build\cxi0\romfs.bin --romfs-dir build\cxi0\romfs --romfs cci\cxi0\romfs.bin
+REM MD build\cfa1
+REM tools\3dstool -cvtf romfs build\cfa1\romfs.bin --romfs-dir cci\cfa1\romfs --romfs cci\cfa1\romfs.bin
+REM MD build\cfa7
+REM tools\3dstool -cvtf romfs build\cfa7\romfs.bin --romfs-dir cci\cfa7\romfs --romfs cci\cfa7\romfs.bin
+REM tools\3dstool -cvtf cxi build\0.cxi --header cci\cxi0\ncchheader.bin --exh cci\cxi0\exh.bin --logo cci\cxi0\logo.darc.lz --plain cci\cxi0\plain.bin --exefs build\cxi0\exefs.bin --romfs build\cxi0\romfs.bin
+tools\3dstool -cvtf cxi build\0.cxi --header cci\cxi0\ncchheader.bin --exh cci\cxi0\exh.bin --logo cci\cxi0\logo.darc.lz --plain cci\cxi0\plain.bin --exefs build\cxi0\exefs.bin --romfs cci\cxi0\romfs.bin
+REM tools\3dstool -cvtf cfa build\1.cfa --header cci\cfa1\ncchheader.bin --romfs build\cfa1\romfs.bin
+REM tools\3dstool -cvtf cfa build\7.cfa --header cci\cfa7\ncchheader.bin --romfs build\cfa7\romfs.bin
+REM tools\3dstool -cvt017f cci build\0.cxi build\1.cfa build\7.cfa "Paper Mario - Sticker Star (TWN) (zh-Hant).3ds" --header cci\ncsdheader.bin
+tools\3dstool -cvt017f cci build\0.cxi cci\1.cfa cci\7.cfa "Paper Mario - Sticker Star (TWN) (zh-Hant).3ds" --header cci\ncsdheader.bin
 IF NOT "%~1"=="" (
 tools\3dstool --diff -vt cci --old "%~1" --new "Paper Mario - Sticker Star (TWN) (zh-Hant).3ds" --patch-file patch.3ps
 tools\icn2ico cci\cxi0\exefs\icon.icn zh\icon.ico
